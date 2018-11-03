dfcacf41c0b6bc48bccb94d73fc4493f9549ed8c
sadness_ojisan <sadness.ojisan@gmail.com>
2018-11-03 16:59:13 +0900

    update: build file to use prd build

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,36 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
+const env = process.env.NODE_ENV;
 
 module.exports = {
-    mode: 'development',
-    entry: './src/main.tsx',
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: 'build.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader'
-        }, 
-        {
-            test: /\.css/,
-            use: [
-              'style-loader',
-              {loader: 'css-loader', options: {url: false}},
-            ],
-          },
-          {
-            test: /\.(jpg|png)$/,
-            loaders: 'url-loader'
-          }
-      ]
-    },
-    resolve: {
-      extensions: [
-        '.ts', '.tsx', '.js', '.json'
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./src/index.html"
-      })
+  mode: env || 'development',
+  entry: './src/main.tsx',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', { loader: 'css-loader', options: { url: false } }]
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loaders: 'url-loader'
+      }
     ]
-  };
-  
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
