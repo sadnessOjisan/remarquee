f2ca6f8b71c80378dead069daad17dd8b2259fc2
sadness_ojisan <sadness.ojisan@gmail.com>
2018-11-03 16:06:36 +0900

    add: .prettierrc & format files

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,8 +1,7 @@
-import * as React from "react";
-import { storiesOf } from '@storybook/react';
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
 import Marquee from '../src/main.tsx'
 
-storiesOf('Button', module)
-  .add('normal marquee', () => 
+storiesOf('Button', module).add('normal marquee', () => (
     <Marquee>aaaaaaaa</Marquee>
-  );
+))
