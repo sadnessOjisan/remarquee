330997c80f6a5685b027b755616f8d90d1c3646b
sadness_ojisan <sadness.ojisan@gmail.com>
2018-11-04 21:53:44 +0900

    update: storybook to add some examples

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -9,33 +9,113 @@ storiesOf('marquee', module)
   .add('<Marquee direction="up">upside</Marquee>', () => (
     <Marquee direction="up">upside</Marquee>
   ))
-  .add('red', () => (
-    <Marquee direction="up" bgcolor="red" height="30px">
-      aaaaaaaa
+  .add('<Marquee direction="right">right</Marquee>', () => (
+    <Marquee direction="right">right</Marquee>
+  ))
+  .add('<Marquee direction="down">down</Marquee>', () => (
+    <Marquee direction="down">down</Marquee>
+  ))
+  .add('<Marquee bgcolor="red">red</Marquee>', () => (
+    <Marquee bgcolor="red">red</Marquee>
+  ))
+  .add('<Marquee bgcolor="blue">blue</Marquee>', () => (
+    <Marquee bgcolor="blue">blue</Marquee>
+  ))
+  .add('<Marquee height="100%" direction="up">height="100%"</Marquee>', () => (
+    <Marquee height="100%" direction="up">
+      height="100%" direction="up"
     </Marquee>
   ))
-  .add('hspace left', () => (
-    <Marquee hspace={55}>hspacehspacehspacehspace</Marquee>
-  ))
-  .add('hspace right', () => (
-    <Marquee hspace={55} direction="right">
-      hspacehspacehspacehspace
+  .add('<Marquee height="70px" direction="up">height="70px"</Marquee>', () => (
+    <Marquee height="70px" direction="up">
+      height="70px" direction="up"
     </Marquee>
   ))
-  .add('vspace up', () => (
-    <Marquee vspace={55} direction="up" height="300px">
-      vspacevspacevspacevspace
+  .add('<Marquee hspace={55}>Marquee hspace={55}</Marquee>', () => (
+    <Marquee hspace={55} width="400px">
+      hspace=
+      {55} width="400px" left
     </Marquee>
   ))
-  .add('vspace down', () => (
-    <Marquee vspace={55} direction="down">
-      vspacevspacevspacevspace
+  .add(
+    '<Marquee vspace={55} direction="up">Marquee vspace={55}</Marquee>',
+    () => (
+      <Marquee vspace={55} height="400px" direction="up">
+        vspace=
+        {55} height="400px" direction="up"
+      </Marquee>
+    )
+  )
+  .add(
+    '<Marquee scrollamount={3}>scrollamount={3}ちょっと遅いよ</Marquee>',
+    () => (
+      <Marquee scrollamount={3}>
+        scrollamount=
+        {3} ちょっと遅いよ
+      </Marquee>
+    )
+  )
+  .add(
+    '<Marquee scrollamount={10}>scrollamount={10}ちょっと早いよ</Marquee>',
+    () => (
+      <Marquee scrollamount={10}>
+        scrollamount=
+        {10} ちょっと早いよ
+      </Marquee>
+    )
+  )
+  .add(
+    '<Marquee scrollamount={300}>scrollamount={300} めっちゃ早いよ</Marquee>',
+    () => (
+      <Marquee scrollamount={300}>
+        scrollamount=
+        {300} めっちゃ早いよ
+      </Marquee>
+    )
+  )
+  .add('<Marquee scrolldelay={3}>scrolldelay={3}3delay</Marquee>', () => (
+    <Marquee scrolldelay={3}>
+      scrolldelay=
+      {3} delay
     </Marquee>
   ))
-  .add('ireko', () => (
+  .add('<Marquee scrolldelay={100}>scrolldelay={100}100delay</Marquee>', () => (
+    <Marquee scrolldelay={100}>
+      scrolldelay=
+      {100} delay
+    </Marquee>
+  ))
+  .add(
+    '<Marquee scrolldelay={1000}>scrolldelay={1000}1000delay</Marquee>',
+    () => (
+      <Marquee scrolldelay={1000}>
+        scrolldelay=
+        {1000} delay
+      </Marquee>
+    )
+  )
+  .add(
+    '<Marquee scrolldelay={3} truespeed="true">scrolldelay={3} truespeed="true"3delay</Marquee>',
+    () => (
+      <Marquee scrolldelay={3} truespeed="true">
+        scrolldelay=
+        {3} truespeed="true" delay
+      </Marquee>
+    )
+  )
+  .add('marquee in marquee', () => (
     <Marquee direction="up" height="400px">
       <Marquee direction="left" width="800px">
-        aaaaaaaa
+        marquee in marquee
+      </Marquee>
+    </Marquee>
+  ))
+  .add('marquee in marquee in marquee', () => (
+    <Marquee direction="up" height="400px">
+      <Marquee direction="left" width="800px">
+        <Marquee direction="right" width="300px">
+          marquee in marquee in marquee
+        </Marquee>
       </Marquee>
     </Marquee>
   ));
